69061a08963fcd67b85efc3a9f894df31c42fd1c
Lukasz Chmielewski <l.chmielewski@sportradar.com>
2023-02-14 12:15:03 +0100

    Changed port

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:9999",
+        target: "http://localhost:8001",
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
